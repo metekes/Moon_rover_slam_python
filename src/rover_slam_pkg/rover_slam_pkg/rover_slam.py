94b3bbf21f57167772af76f8d8eeaa68287f3c23
metekes <metekesici@gmail.com>
2025-02-05 13:19:40 +0300

    Loop closure is added, it is not working properly

--- src/rover_slam_pkg/rover_slam_pkg/rover_slam.py
+++ src/rover_slam_pkg/rover_slam_pkg/rover_slam.py
@@ -12,6 +12,8 @@ from gazebo_msgs.msg import ModelStates
 import g2o
 import cv2 as cv
 import numpy as np
+import joblib
+from sklearn.metrics.pairwise import cosine_similarity
 
 from .helper import *
 from .customVertexSE3 import *
@@ -38,16 +40,19 @@ class GraphSLAM(Node):
                                               [0,      391.64, 180.5],
                                               [0,      0,      1]])
         self.frame_count_from_keyframe_ = 0
-        self.keyframe_list_ = []
-        self.feature_match_distance_threshold_ = 30 # should be tuned
+        self.keyframe_histogram_dict_ = {}
+        self.keyframe_keypoint_dict_ = {}
+        self.keyframe_descriptor_dict_ = {}
+        self.feature_match_distance_threshold_ = 35 # should be tuned
         self.max_feature_3D_distance_ = 7 # should be tuned
         self.num_closest_feature_cooridantes_ = 15 # should be tuned
-        self.max_depth_measurable_ = 8 # should be tuned
+        self.max_depth_measurable_ = 40 # should be tuned
         self.img_width_ = 0
         self.img_height_ = 0
+        camera_tilt_angle = 0 # in radians
         self.transformation_correction_camera_tilt_ = np.array([[ 1,  0, 0, 0],
-                                                                [ 0,  np.cos(0.4), np.sin(0.4), 0],
-                                                                [ 0, -np.sin(0.4), np.cos(0.4), 0],
+                                                                [ 0,  np.cos(camera_tilt_angle), np.sin(camera_tilt_angle), 0],
+                                                                [ 0, -np.sin(camera_tilt_angle), np.cos(camera_tilt_angle), 0],
                                                                 [ 0,  0, 0, 1]])
         
         self.transformation_from_camera_to_world_ = self.transformation_correction_camera_tilt_ @ np.array([[ 0,  0, 1,   0.215],
@@ -70,6 +75,9 @@ class GraphSLAM(Node):
         self.kernel_ = g2o.RobustKernelTukey() # decide which kernel to use and its threshold
         self.kernel_.set_delta(5)
         self.rover_vertex_ids_ = set()
+
+        self.kmeans_ = joblib.load('/home/mete/rover_slam_python/kmeans_model.pkl')
+        self.tfidf_transformer_ = joblib.load('/home/mete/rover_slam_python/tfidf_transformer.pkl')
 
         # set topic names
         self.rgb_img_topic_   = "/camera/image_raw"
@@ -141,11 +149,13 @@ class GraphSLAM(Node):
             self.img_height_ = np.shape(self.current_rgb_img_)[0]
             self.img_width_ = np.shape(self.current_rgb_img_)[1]
             self.current_img_time_ = rgb_img_msg.header.stamp.sec + rgb_img_msg.header.stamp.nanosec * 10**-9
-            path = "/home/mete/rover_slam_python/photos/" + str(self.count_) + ".jpg" 
-            cv.imwrite(path, self.current_rgb_img_)
+            # path = "/home/mete/rover_slam_python/photos/" + str(self.count_) + ".jpg" 
+            # cv.imwrite(path, self.current_rgb_img_)
             # self.decodePointCloud(pointcloud_msg)
             self.frame_count_from_keyframe_ += 1
 
+            cv.imshow("rgb", self.current_rgb_img_)
+            cv.waitKey(1)
             """
             # TO VISUALIZE THE IMGS
             print(self.current_rgb_img_.shape)
@@ -188,45 +198,76 @@ class GraphSLAM(Node):
         if (not self.flag_initial_imu_data_available_):
             self.resetIMUData()
         
+        # imu only
         elif ((not self.flag_img_available_) and self.flag_imu_data_available_):
             self.runOdometry()
             self.resetIMUData()
+            return
 
+        # camera only
         elif ((not self.flag_imu_data_available_) and self.flag_img_available_):
             if (not self.flag_initial_img_available_):
                 orb = cv.ORB_create()
-                _, kf_descriptors = orb.detectAndCompute(cv.cvtColor(self.current_rgb_img_, cv.COLOR_BGR2GRAY), None)
-                self.keyframe_list_.append(kf_descriptors.copy())
+                kf_keypoints, kf_descriptors = orb.detectAndCompute(cv.cvtColor(self.current_rgb_img_, cv.COLOR_BGR2GRAY), None)
+                self.keyframe_histogram_dict_[self.current_img_time_*1000] = self.createBoWHistogram(kf_descriptors)
+                self.keyframe_keypoint_dict_[self.current_img_time_*1000] = kf_keypoints
+                self.keyframe_descriptor_dict_[self.current_img_time_*1000] = kf_descriptors.copy()
+                self.last_kf_descriptor = kf_descriptors.copy()
                 self.resetImgData()
                 return
             
             self.extractFeatures()
             self.matchFeatures()
-            if (not self.flag_matches_available_): return
-            self.getFeatureCoordinatesIn3D()
-            if (not self.flag_keypoint_coordinates_available_): return
-            self.runICP() # visiual-inertial odometry
 
+            if (not self.flag_matches_available_):
+                self.detectLoopClosure()
+                self.resetImgData()
+                return
+            
+            self.getFeatureCoordinatesIn3D()
+            if (not self.flag_keypoint_coordinates_available_):
+                self.detectLoopClosure()
+                self.resetImgData()
+                return
+            
+            self.runICP() # visiual-inertial odometry
+            self.detectLoopClosure()
             self.resetImgData()
         
+        # both imu and camera
         elif (self.flag_imu_data_available_ and self.flag_img_available_):
             self.runOdometry()
-            self.resetIMUData()
 
             if (not self.flag_initial_img_available_):
                 orb = cv.ORB_create()
-                _, kf_descriptors = orb.detectAndCompute(cv.cvtColor(self.current_rgb_img_, cv.COLOR_BGR2GRAY), None)
-                self.keyframe_list_.append(kf_descriptors.copy())
+                kf_keypoints, kf_descriptors = orb.detectAndCompute(cv.cvtColor(self.current_rgb_img_, cv.COLOR_BGR2GRAY), None)
+                self.keyframe_histogram_dict_[self.current_img_time_*1000] = self.createBoWHistogram(kf_descriptors)
+                self.keyframe_keypoint_dict_[self.current_img_time_*1000] = kf_keypoints
+                self.keyframe_descriptor_dict_[self.current_img_time_*1000] = kf_descriptors.copy()
+                self.last_kf_descriptor = kf_descriptors.copy()
+                self.resetIMUData()
                 self.resetImgData()
                 return
+            
             self.extractFeatures()
             self.matchFeatures()
-            if (not self.flag_matches_available_): return
+
+            if (not self.flag_matches_available_):
+                self.detectLoopClosure()
+                self.resetIMUData()
+                self.resetImgData()
+                return
+            
             self.getFeatureCoordinatesIn3D()
-            if (not self.flag_keypoint_coordinates_available_): return
+            if (not self.flag_keypoint_coordinates_available_):
+                self.detectLoopClosure()
+                self.resetIMUData()
+                self.resetImgData()
+                return
 
             self.runICP() # visiual-inertial odometry
-
+            self.detectLoopClosure()
+            self.resetIMUData()
             self.resetImgData()
             # self.prev_descriptors_ = self.current_descriptors_
             # self.prev_good_depths_ = self.current_good_depths_ ####################################
@@ -278,18 +319,32 @@ class GraphSLAM(Node):
         cv.waitKey(1)
         """
 
+    def createBoWHistogram(self, descriptors):
+        if descriptors is None:
+            return np.zeros(self.kmeans_.n_clusters)
+        visual_words = self.kmeans_.predict(descriptors)
+        histogram, _ = np.histogram(visual_words, bins=np.arange(self.kmeans_.n_clusters + 1))
+        histogram = self.tfidf_transformer_.transform(histogram.reshape(1, -1)).toarray()
+        return histogram
+
     def keyframeSelection(self):
-        if self.frame_count_from_keyframe_ >= 20 and self.current_descriptors_ is not None:
-            self.keyframe_list_.append(self.current_descriptors_.copy())
+        if self.frame_count_from_keyframe_ >= 1000 and self.current_descriptors_ is not None:
+            self.keyframe_histogram_dict_[self.current_img_time_*1000] = self.createBoWHistogram(self.current_descriptors_)
+            self.keyframe_keypoint_dict_[self.current_img_time_*1000] = self.current_keypoints_
+            self.keyframe_descriptor_dict_[self.current_img_time_*1000] = self.current_descriptors_.copy()
             self.frame_count_from_keyframe_ = 0
+            self.last_kf_descriptor = self.current_descriptors_.copy()
         
-        elif len(self.keyframe_list_) != 0 and self.current_descriptors_ is not None:
+        elif len(self.keyframe_histogram_dict_) != 0 and self.current_descriptors_ is not None:
             bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
-            matches = bf.match(self.keyframe_list_[-1], self.current_descriptors_)
+            matches = bf.match(self.last_kf_descriptor, self.current_descriptors_)
             good_matches = [m for m in matches if m.distance < self.feature_match_distance_threshold_]
-            if len(good_matches) < 0.9 * len(self.current_descriptors_):
-                self.keyframe_list_.append(self.current_descriptors_.copy())
+            if len(good_matches) < 0.7 * len(self.current_descriptors_):
+                self.keyframe_histogram_dict_[self.current_img_time_*1000] = self.createBoWHistogram(self.current_descriptors_)
+                self.keyframe_keypoint_dict_[self.current_img_time_*1000] = self.current_keypoints_
+                self.keyframe_descriptor_dict_[self.current_img_time_*1000] = self.current_descriptors_.copy()
                 self.frame_count_from_keyframe_ = 0
+                self.last_kf_descriptor = self.current_descriptors_.copy()
 
     def getFeatureCoordinatesIn3D(self):
         self.prev_feature_coordinates_ = []
@@ -312,12 +367,12 @@ class GraphSLAM(Node):
             position = self.transformation_from_camera_to_world_ @ np.vstack((position.reshape(3,1), np.array([1])))
             self.current_feature_coordinates_.append(position[:3])
 
-        # find features to seperate from each other in 3D
+        # find features too seperate from each other in 3D
         for ind in range(len(self.prev_feature_coordinates_)):
             dist = np.linalg.norm(self.prev_feature_coordinates_[ind] - self.current_feature_coordinates_[ind])
             if dist >= self.max_feature_3D_distance_: indices_to_remove.add(ind)
         
-        # remove features further away from maximum measurable distance and the features two seperate from each other in 3D
+        # remove features further away from maximum measurable distance and the features too seperate from each other in 3D
         for ind in sorted(indices_to_remove, reverse=True):
             del self.prev_feature_coordinates_[ind]
             del self.current_feature_coordinates_[ind]
@@ -350,36 +405,14 @@ class GraphSLAM(Node):
         # [x,y,z] = self.current_feature_coordinates_ in the world frame not in camera
         # for the camera frame: origin at the center, z outside the camera, x positive towards right (along width), y positive towards down (along height)
 
-    def runICP(self):
+    def runICP(self, pose1=None, pose2=None):
         
-        """
-        # To test if the performace degrades when the num of features are less than 6
-        count = 0
-        for i in range(len(self.prev_feature_coordinates_)):
-            dist = self.prev_feature_coordinates_[i] - self.current_feature_coordinates_[i]
-            if np.linalg.norm(dist) < 3: count = count + 1
-        
-        if count < 6: return
-        print(count)
-        """
-
-        """
-        for i in range(len(self.prev_feature_coordinates_)):
-            dist = self.prev_feature_coordinates_[i] - self.current_feature_coordinates_[i]
-            if np.linalg.norm(dist) < 2:
-                print("-------------------------------")
-                print(self.prev_feature_coordinates_[i])
-                print(self.current_feature_coordinates_[i])
-                print("-------------------------------")
-        """
-        """
-        print(len(self.prev_feature_coordinates_))
-        if len(self.prev_feature_coordinates_) >= 6:
-            print(self.prev_feature_coordinates_[:5])
-            print(self.current_feature_coordinates_[:5])
-        """
         # add incremental poses
-        if (not self.flag_imu_data_available_):
+        if(pose1 is not None and pose2 is not None):
+            pose_1 = pose1
+            pose_2 = pose2
+
+        elif (not self.flag_imu_data_available_):
             pose_1, pose_2 = self.setImgPoses()
         
         else:
@@ -432,8 +465,8 @@ class GraphSLAM(Node):
         self.optimizer_.initialize_optimization()
         # self.optimizer_.set_verbose(True)
         self.optimizer_.optimize(20)
-        pose_1.set_fixed((True))
-        pose_2.set_fixed((True))
+        pose_1.set_fixed(True)
+        pose_2.set_fixed(True)
 
         # store optimized poses
         optimized_pose2 = pose_2.estimate()
@@ -455,6 +488,8 @@ class GraphSLAM(Node):
             pose_1.set_id(number_of_vertices)
             pose_1.set_estimate(g2o.Isometry3d(quaternion2rotation_matrix(self.gt_rover_orientation_.reshape(4,1)).transpose(), self.gt_rover_position_))
             self.rover_vertex_ids_.add(pose_1.id())
+            self.optimizer_.add_vertex(pose_1)
+            pose_1.set_timestamp(self.prev_img_time_*1000)
         
         else:
             pose_1 = self.optimizer_.vertex(self.last_node_id_)
@@ -467,6 +502,7 @@ class GraphSLAM(Node):
         self.optimizer_.add_vertex(pose_2)
         self.last_node_id_ = number_of_vertices + 1
         self.rover_vertex_ids_.add(pose_2.id())
+        pose_2.set_timestamp(self.current_img_time_*1000)
         return pose_1, pose_2
 
     def matchIMUandImgTimes(self):
@@ -547,14 +583,14 @@ class GraphSLAM(Node):
         self.optimizer_.initialize_optimization()
         # self.optimizer_.set_verbose(True)
         self.optimizer_.optimize(10)
-        pose_1.set_fixed((True))
-        pose_2.set_fixed((True))
+        pose_1.set_fixed(True)
+        pose_2.set_fixed(True)
 
         # store poses
         optimized_pose2 = pose_2.estimate()
         self.file_.write(f"{self.gt_rover_position_[0]}, {self.gt_rover_position_[1]}, {self.gt_rover_position_[2]}, {optimized_pose2.translation()[0]}, {optimized_pose2.translation()[1]}, {optimized_pose2.translation()[2]}\n")
 
-        if self.count_ % 20 == 0:
+        if self.count_ % 100 == 0:
             print("Optimized Pose:")
             print("IMU")
             print(f"gt: {self.gt_rover_position_}")
@@ -577,19 +613,74 @@ class GraphSLAM(Node):
 
         self.orientation_quat_ += quaternion_derivative(angular_vel, self.orientation_quat_) * dt
         self.orientation_quat_ = self.orientation_quat_ / np.linalg.norm(self.orientation_quat_)
+    
+    def detectLoopClosure(self):
+        current_frame_histogram = self.createBoWHistogram(self.current_descriptors_.copy())
+        # Apply TF-IDF transformation
+        current_frame_tfidf = self.tfidf_transformer_.transform(current_frame_histogram.reshape(1, -1)).toarray()
+
+        threshold = 0.8
+        self.best_loop_closure_timestamp = None
+        best_score = 0
+
+        for frame_timestamp, keyframe_tfidf in self.keyframe_histogram_dict_.items():
+            # Compute cosine similarity
+            similarity = cosine_similarity(current_frame_tfidf, keyframe_tfidf)[0, 0]
+
+            # Check if similarity is above the threshold
+            if similarity > threshold and similarity > best_score:
+                if abs(frame_timestamp - self.current_img_time_*1000)/1000 < 5: break
+                self.best_loop_closure_timestamp = frame_timestamp
+                best_score = similarity
+
+        if best_score > threshold:
+            print("Loop closure detected\n")
+            self.closeLoop()
+
+    def closeLoop(self):
+        closest_pose_1 = None
+        closest_time_diff = float('inf')
+
+        for pose_id in self.rover_vertex_ids_:
+            vertex = self.optimizer_.vertex(pose_id)
+
+            # Calculate the time difference
+            time_diff = abs(vertex.get_timestamp() - self.best_loop_closure_timestamp)
+            
+            # Update the closest vertex if this one is closer
+            if time_diff < closest_time_diff:
+                closest_pose_1 = vertex
+                closest_time_diff = time_diff
+
+        closest_pose_2 = None
+        closest_time_diff = float('inf')
+
+        for pose_id in self.rover_vertex_ids_:
+            vertex = self.optimizer_.vertex(pose_id)
+
+            # Calculate the time difference
+            time_diff = abs(vertex.get_timestamp() - self.current_img_time_*1000)
+            
+            # Update the closest vertex if this one is closer
+            if time_diff < closest_time_diff:
+                closest_pose_2 = vertex
+                closest_time_diff = time_diff
+
+        closest_pose_1.set_fixed(True)
+        closest_pose_2.set_fixed(False)
+
+        self.prev_keypoints_ = self.keyframe_keypoint_dict_[self.best_loop_closure_timestamp]
+        self.prev_descriptors_ = self.keyframe_descriptor_dict_[self.best_loop_closure_timestamp]
+
+        self.matchFeatures()
+        if (not self.flag_matches_available_):
+            return
         
-        """
-        ##########################################################################################
-        if self.count_ % 100 == 0:
-            # print(acc_inertial)
-            print(f"gt: {self.gt_rover_position_}")
-            print(f"estimated: {self.position_}")
-            print(f"error: {self.gt_rover_position_.reshape(3,1) - self.position_}")
-            # print(optimized_pose2.matrix())
-            print("\n")
-        return
-        ###########################################################################################
-        """
+        self.getFeatureCoordinatesIn3D()
+        if (not self.flag_keypoint_coordinates_available_):
+            return
+        
+        self.runICP(closest_pose_1, closest_pose_2)
 
 def main(args=None):
     rclpy.init(args=args)
